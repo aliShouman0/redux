9d2e6cf3200ed1c5cf99dd585a0cbc649880a5a6
Ali Alrida Shouman <alishouman010@gmail.com>
2023-02-10 11:05:35 +0200

    Adding fetch to get data from fake api

--- 2-redux-and-react/src/components/Posts.js
+++ 2-redux-and-react/src/components/Posts.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function Posts() {
+  const [mounted, setMounted] = useState(false);
+  const [posts, setPosts] = useState([]);
+
+  if (!mounted) {
+    // Code for componentWillMount here
+    // This code is called only one time before intial render
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  }
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  
   return (
-    <div>Posts</div>
-  )
+    <div>
+      <h1>Posts</h1>
+    </div>
+  );
 }
 
-export default Posts
+export default Posts;
