3a3f860398e364bbfead5a3c5a6d50226afee659
Ali Alrida Shouman <alishouman010@gmail.com>
2023-02-13 00:14:12 +0200

    Adding Provider for redux in react app

--- 2-redux-and-react/src/App.js
+++ 2-redux-and-react/src/App.js
@@ -2,17 +2,22 @@ import logo from "./logo.svg";
 import "./App.css";
 import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
+import { Provider } from "react-redux"; 
+import store from "./redux/store";
+// configureStore
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <PostForm />
-      <hr />
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <PostForm />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
