ec487a52695446479a185a3c006f5e550a7add1f
Ali Alrida Shouman <alishouman010@gmail.com>
2023-02-13 00:15:40 +0200

    Adding posts from the redux store on the website

--- 2-redux-and-react/src/components/Posts.jsx
+++ 2-redux-and-react/src/components/Posts.jsx
@@ -1,20 +1,27 @@
 import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../redux/actions/postAction";
 
-function Posts() {
-  const [mounted, setMounted] = useState(false);
-  const [posts, setPosts] = useState([]);
-
-  if (!mounted) {
-    // Code for componentWillMount here
-    // This code is called only one time before intial render
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((data) => setPosts(data));
-  }
+function Posts(props) {
+  const [posts, setPosts] = useState(props.posts);
+  // const [newPost, setNewPost] = useState(props.newPost)
 
   useEffect(() => {
-    setMounted(true);
+    props.fetchPosts();
   }, []);
+
+  useEffect(() => {
+    // props.fetchPosts();
+    if (props.posts) {
+      setPosts(props.posts);
+    }
+  }, [props.posts]);
+
+  useEffect(() => {
+    if (props.newPost) {
+      setPosts([...posts, props.newPost]);
+    }
+  }, [props.newPost]);
 
   const PostItem = posts.map((post) => (
     <div key={post.id}>
@@ -31,4 +38,9 @@ function Posts() {
   );
 }
 
-export default Posts;
+const mapStateToProps = (state) => ({
+  posts: state.posts.items,
+  newPost: state.posts.item,
+});
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
