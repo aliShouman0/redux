fede675b8d0a520e34b2d49b4d393970209164bd
Ali Alrida Shouman <alishouman010@gmail.com>
2023-02-14 20:18:58 +0200

    Creating reservation component

---
+++ 3-redux-toolkit-and-react/src/Components/ReservationCard.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+interface ReservationCardTypes {
+  name: string;
+}
+
+function ReservationCard({ name }: ReservationCardTypes) {
+  return <div className="reservation-card-container">{name}</div>;
+}
+
+export default ReservationCard;
