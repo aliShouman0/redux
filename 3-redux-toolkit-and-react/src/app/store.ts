8e36d5dea7b0df935ea60ba5d4664690624d3672
Ali Alrida Shouman <alishouman010@gmail.com>
2023-02-15 18:58:52 +0200

    Adding customer Reducer to the store

--- 3-redux-toolkit-and-react/src/app/store.ts
+++ 3-redux-toolkit-and-react/src/app/store.ts
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import reservationSlice from "../features/reservationSlice";
+import customerReducer from "../features/customerSlice";
+import reservationReducer from "../features/reservationSlice";
 
 export const store = configureStore({
   reducer: {
-    reservations:reservationSlice
+    reservations:reservationReducer,
+    customer:customerReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch=typeof store.dispatch;
+                             
