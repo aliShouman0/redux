56d29a899e2db5e3b00d734bf6e0c3e9933e6661
Ali Alrida Shouman <alishouman010@gmail.com>
2023-02-15 19:00:03 +0200

    Finalizing UI

--- 3-redux-toolkit-and-react/src/App.tsx
+++ 3-redux-toolkit-and-react/src/App.tsx
@@ -1,7 +1,27 @@
 import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import "./App.css";
+import { RootState } from "./app/store";
+import CustomerCard from "./Components/CustomerCard";
+import ReservationCard from "./Components/ReservationCard";
+import { addReservation } from "./features/reservationSlice";
 
 function App() {
+  const [reservationName, setReservationName] = useState("");
+
+  const reservation = useSelector(
+    (state: RootState) => state.reservations.value
+  );
+
+  const customers = useSelector((state: RootState) => state.customer.value);
+
+  const dispatch = useDispatch();
+  const addName = () => {
+    if (!reservationName) return;
+    dispatch(addReservation(reservationName));
+    setReservationName("");
+  };
+
   return (
     <div className="App">
       <div className="container">
@@ -9,25 +29,31 @@ function App() {
           <div>
             <h5 className="reservation-header">Reservations</h5>
             <div className="reservation-cards-container">
-              <div className="reservation-card-container">Laith Harb</div>
+              {reservation.map((name, idx) => (
+                <ReservationCard key={idx} name={name} index={idx} />
+              ))}
             </div>
           </div>
           <div className="reservation-input-container">
-            <input />
-            <button>Add</button>
+            <input
+              type="text"
+              value={reservationName}
+              onChange={(e) => {
+                setReservationName(e.target.value);
+              }}
+            />
+            <button onClick={addName}>Add</button>
           </div>
         </div>
         <div className="customer-food-container">
-          <div className="customer-food-card-container">
-            <p>Selena Gomez</p>
-            <div className="customer-foods-container">
-              <div className="customer-food"></div>
-              <div className="customer-food-input-container">
-                <input />
-                <button>Add</button>
-              </div>
-            </div>
-          </div>
+          {customers.map((customer) => (
+            <CustomerCard
+              name={customer.name}
+              food={customer.food}
+              id={customer.id}
+              key={customer.id}
+            />
+          ))}
         </div>
       </div>
     </div>
