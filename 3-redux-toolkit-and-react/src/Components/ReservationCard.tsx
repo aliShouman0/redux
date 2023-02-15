05e3302ac3aca34679447652a7122f8896843516
Ali Alrida Shouman <alishouman010@gmail.com>
2023-02-15 18:56:51 +0200

    Editing Reservation Card Components

--- 3-redux-toolkit-and-react/src/Components/ReservationCard.tsx
+++ 3-redux-toolkit-and-react/src/Components/ReservationCard.tsx
@@ -1,10 +1,41 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { v4 as uuid } from "uuid";
+import { addCustomer } from "../features/customerSlice";
+import { removeReservation } from "../features/reservationSlice";
+
 interface ReservationCardTypes {
   name: string;
+  index: number;
 }
 
-function ReservationCard({ name }: ReservationCardTypes) {
-  return <div className="reservation-card-container">{name}</div>;
+interface Customer {
+  id: string;
+  name: string;
+  food: string[];
+}
+
+function ReservationCard({ name, index }: ReservationCardTypes) {
+  const dispatch = useDispatch();
+  const dispatch2 = useDispatch();
+
+  const customer: Customer = {
+    id: uuid(),
+    name,
+    food: [],
+  };
+
+  return (
+    <div
+      onClick={() => {
+        dispatch(removeReservation(index));
+        dispatch2(addCustomer(customer));
+      }}
+      className="reservation-card-container"
+    >
+      {name}
+    </div>
+  );
 }
 
 export default ReservationCard;
